f084540f133a01a20755662cb519365e65acdd75
Jorinda Becker <jorindabecker@gmail.com>
2021-02-22 14:39:48 -0700

    new issue. editable webpages like a word document

--- js/content.js
+++ js/content.js
@@ -1,20 +1,21 @@
 console.log("extension is running!")
 
 
-//this works but it changes all text to green
-//on click rather than just what I want to select. 
-//unsure if there is a event listener that works
-//list the css selector.
-// $('p').on("click", function() {
-// 	$('p').css({"color":"green"});
-// })
-
-// console.log("clicked")
-
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		var color = request.message;
-		$("p").css("color", color)
+// chrome.runtime.onMessage.addListener(
+// 	function(request, sender, sendResponse) {
+// 		var color = request.message;
+// 		$("p").css("color", color)
 		
-	}
-);
+// 	}
+// );
+document.designMode = "on"; 
+
+
+
+$("p").attr("id", "textToSelect");
+
+console.log("paragraph id has changed")
+document.getElementById("textToSelect").addEventListener('mouseup', function(e){
+	document.execCommand('hiliteColor', false, 'yellow')
+}); 
+
--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,16 @@
 console.log("I'm the popup")
 
-// $("#buttonred").on("click", function() {
+//linked to buttons
+// $('#blue').on('click', changeTheme);
+// $('#pink').on('click', changeTheme);
+// $('#green').on('click', changeTheme);
+// $('#yellow').on('click', changeTheme);
 
+// function changeTheme () {
+// 	var color = $(this).attr('id');
+// 	//send message to the DOM of active tab
 // 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-// 		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"})
-// 	})
-// })
+// 		chrome.tabs.sendMessage(tabs[0].id, {message: color});
+// 	});
+// }
 
-$('#blue').on('click', changeTheme);
-$('#pink').on('click', changeTheme);
-$('#green').on('click', changeTheme);
-$('#yellow').on('click', changeTheme);
-
-function changeTheme () {
-	var color = $(this).attr('id');
-	//send message to the DOM of active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {message: color});
-	});
-}

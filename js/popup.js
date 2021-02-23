23166e8feed08c535037e601b7d7adf6682ea7f6
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2021-02-22 23:27:32 -0500

    Rash playing with the code

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,11 @@
 console.log("I'm the popup")
 
 //linked to buttons
-$('#deepskyblue').on('click', changeTheme);
-$('#lightblue').on('click', changeTheme);
-$('#pink').on('click', changeTheme);
-$('#lightgreen').on('click', changeTheme);
-$('#yellow').on('click', changeTheme);
-$('#orange').on('click', changeTheme);
-$('#deeppink').on('click', changeTheme);
-$('#turquoise').on('click', changeTheme);
-$('#navajowhite').on('click', changeTheme);
+$(".themebutton").each( (_,k) => { $(k).on('click', changeTheme)})
 
 function changeTheme () {
-	var color = $(this).attr('id');
+	var color = $(this).attr('data-color');
+
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	chrome.tabs.sendMessage(tabs[0].id, {message: color});
 	});
